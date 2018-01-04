1699aed320815aa2bfbb74295a53fffed9141e85
PJ Frias <peejfrias@gmail.com>
2018-01-04 22:58:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
     if value < smallest_value
       smallest_value = value
       smallest_key = key
-      binding.pry
+      # binding.pry
     end
   end
   smallest_key
