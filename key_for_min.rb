01a0937fe516d1c9aa436eea10df326eaf5791b3
PJ Frias <peejfrias@gmail.com>
2018-01-04 22:53:49 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,12 @@ def key_for_min_value(name_hash)
   smallest_key = 1000000
   smallest_value = 1000000
   if name_hash = {}
-    return nil
-  end
-  name_hash.each do |key, value|
-    if value < smallest_value
-      smallest_key = key
+    nil
+  else
+    name_hash.each do |key, value|
+      if value < smallest_value
+        smallest_key = key
+      end
     end
   end
   smallest_key
